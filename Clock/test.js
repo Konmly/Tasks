054aaaf9777c0fd16a70c57a5a9aa8ecf75042ec
Kristina <kristino4ka_17@mail.ru>
2023-04-20 22:35:15 +0300

    clock

---
+++ Clock/test.js
@@ -0,0 +1,28 @@
+let intervalId = null;
+const time = document.querySelector(".counter");
+
+function getClockTick() {
+  let clock = new Date();
+  time.textContent = clock.toLocaleTimeString();
+}
+
+const buttonStart = document.querySelector(".button-start");
+
+function clockStart() {
+  if (intervalId === null) {
+    intervalId = setInterval(getClockTick, 1000);
+  }
+}
+
+buttonStart.addEventListener("click", clockStart);
+
+const buttonStop = document.querySelector(".button-stop");
+
+function clockStop() {
+  if (intervalId === null) {
+    return;
+  }
+  clearInterval(intervalId);
+  intervalId = null;
+}
+buttonStop.addEventListener("click", clockStop);
