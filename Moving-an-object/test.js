d49b54e9b004dd61e2ebc22cdc8120a3ff766447
Kristina <kristino4ka_17@mail.ru>
2023-05-12 19:34:27 +0300

    moving-an-object

---
+++ Moving-an-object/test.js
@@ -0,0 +1,36 @@
+const staticBlock = document.querySelector(".static-block");
+const floatingBlock = document.querySelector(".floating-block");
+
+function objectPositioning(block, position, elem) {
+  const coords = block.getBoundingClientRect();
+
+  switch (position) {
+    case "topLeftCorner":
+      elem.style.left = `${coords.left}px`; // левый верхний внутренний угол
+      break;
+
+    case "topRightCorner":
+      elem.style.top = `${coords.top}px`; // правый верхний  внутренний угол
+      elem.style.left = `${
+        coords.left + block.offsetWidth - elem.offsetWidth
+      }px`;
+      break;
+
+    case "bottomRightCorner":
+      elem.style.left = `${
+        coords.left + block.offsetWidth - elem.offsetWidth
+      }px`; // правый нижний внутренний угол
+      elem.style.top = `${
+        coords.top + block.offsetHeight - elem.offsetHeight
+      }px`;
+      break;
+
+    case "lowerLeftCorner":
+      elem.style.top = `${
+        coords.top + block.offsetHeight - elem.offsetHeight
+      }px`; // левый нижний внутренний угол
+      elem.style.left = `${coords.left}px`;
+      break;
+  }
+}
+objectPositioning(staticBlock, "bottomRightCorner", floatingBlock);
