8b823043708772cccfb0be16207d7702f28778c1
Kristina <kristino4ka_17@mail.ru>
2023-05-18 22:34:48 +0300

    correct the name of the variables, add free letters :)

--- Slider-of-pictures/test.js
+++ Slider-of-pictures/test.js
@@ -1,32 +1,30 @@
-const wrapImg = document.querySelector(".wrap-img");
+const wrapImage = document.querySelector(".wrap-img");
 
 const wrap = document.querySelector(".wrap");
 
-const img = document.querySelector(".img");
-
-const container = document.querySelector(".container");
+const image = document.querySelector(".img");
 
 const buttonRight = document.querySelector(".arrow-right");
 const buttonLeft = document.querySelector(".arrow-left");
 
 let offset = 0;
 
-buttonRight.addEventListener("click", function () {
-  offset = offset + img.offsetWidth;
+buttonRight.addEventListener("click", () => {
+  offset += image.offsetWidth;
 
-  if (offset > wrapImg.offsetWidth - wrap.clientWidth) {
+  if (offset > wrapImage.offsetWidth - wrap.clientWidth) {
     offset = 0;
   }
 
-  wrapImg.style.left = `${-offset}px`;
+  wrapImage.style.left = `${-offset}px`;
 });
 
-buttonLeft.addEventListener("click", function () {
-  offset = offset - img.offsetWidth;
+buttonLeft.addEventListener("click", () => {
+  offset -= image.offsetWidth;
 
   if (offset < 0) {
-    offset = wrapImg.offsetWidth - wrap.clientWidth;
+    offset = wrapImage.offsetWidth - wrap.clientWidth;
   }
 
-  wrapImg.style.left = `${-offset}px`;
+  wrapImage.style.left = `${-offset}px`;
 });
