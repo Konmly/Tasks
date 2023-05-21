b18c187c1c6f3eeb82ed84ebf18d43dc412ba639
Kristina <kristino4ka_17@mail.ru>
2023-05-21 15:43:15 +0300

    change to the toggle method.

--- Open-and-close-the-submenu-with-a-click/test.js
+++ Open-and-close-the-submenu-with-a-click/test.js
@@ -3,14 +3,8 @@ const sublist = document.querySelector(".sublist");
 const spriteRightArrow = document.querySelector(".sprite-right-arrow");
 const spriteLeftArrow = document.querySelector(".sprite-left-arrow");
 
-list.addEventListener("click", function() {
-    if(sublist.classList.contains("none")) {
-        spriteRightArrow.classList.add("none");
-        sublist.classList.remove("none");
-        spriteLeftArrow.classList.remove("none");
-    } else {
-        spriteLeftArrow.classList.add("none");
-        sublist.classList.add("none");
-        spriteRightArrow.classList.remove("none");
-    }
+list.addEventListener("click", () => {
+  spriteRightArrow.classList.toggle("none");
+  sublist.classList.toggle("none");
+  spriteLeftArrow.classList.toggle("none");
 });
