09021173558d8a41c7f757fd9bd4eea27321ea93
Kristina <kristino4ka_17@mail.ru>
2023-05-21 17:19:10 +0300

    change the names of the variables

--- Sorting-the-table/test.js
+++ Sorting-the-table/test.js
@@ -1,13 +1,13 @@
 function sortTable() {
-  const tr = document.querySelector(".tr");
-
   const table = document.querySelector(".table");
 
-  let arr = Array.from(table.rows).slice(1);
+  let rowsList = Array.from(table.rows).slice(1);
 
-  arr.sort((a, b) => (a.cells[0].innerHTML > b.cells[0].innerHTML ? 1 : -1));
+  rowsList.sort((first, second) =>
+    first.cells[0].innerHTML > second.cells[0].innerHTML ? 1 : -1
+  );
 
-  table.tBodies[0].append(...arr);
+  table.tBodies[0].append(...rowsList);
 }
 
 sortTable();
