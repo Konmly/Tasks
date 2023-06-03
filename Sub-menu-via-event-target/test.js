2ef1fc122efa6957c8c5680a0244d0328f744d72
Kristina <kristino4ka_17@mail.ru>
2023-06-03 20:23:29 +0300

    change the name of the class

--- Sub-menu-via-event-target/test.js
+++ Sub-menu-via-event-target/test.js
@@ -4,7 +4,7 @@ const li = ul.querySelectorAll("li");
 
 li.forEach((item) => {
   const span = document.createElement("SPAN");
-  span.classList.add("span");
+  span.classList.add("wrap-list-hide");
   item.prepend(span); // добавили перед тегом span
   span.append(span.nextSibling); // добавили после тега span
 });
@@ -12,7 +12,7 @@ li.forEach((item) => {
 ul.addEventListener("click", (event) => {
   const children = event.target.closest("ul").querySelector("ul"); // у li, что мы нашли находим родителя, и у родителя ul внутри
 
-  if (!event.target.classList.contains("span")) {
+  if (!event.target.classList.contains("wrap-list-hide")) {
     return;
   }
 
