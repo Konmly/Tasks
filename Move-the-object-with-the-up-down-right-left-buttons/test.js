ba70e9889d17b9ee9558a78283d17f253ec35859
Kristina <kristino4ka_17@mail.ru>
2023-06-30 20:18:55 +0300

    move-the-object-with-the-up-down-right-left-buttons

---
+++ Move-the-object-with-the-up-down-right-left-buttons/test.js
@@ -0,0 +1,35 @@
+const mouseSprite = document.querySelector(".mouse");
+
+//не забыть включить tabIndex, я добавила в html, но можно и в js, без него не возьмется фокусировка
+
+mouseSprite.addEventListener("click", () => {
+  mouseSprite.style.position = "absolute";
+});
+
+mouseSprite.addEventListener("keydown", (event) => {
+  switch (event.code) {
+    case "Numpad8":
+      mouseSprite.style.top = `${
+        mouseSprite.getBoundingClientRect().top - mouseSprite.offsetHeight
+      }px`;
+      break;
+
+    case "Numpad6":
+      mouseSprite.style.left = `${
+        mouseSprite.getBoundingClientRect().left + mouseSprite.offsetWidth
+      }px`;
+      break;
+
+    case "Numpad2":
+      mouseSprite.style.top = `${
+        mouseSprite.getBoundingClientRect().top + mouseSprite.offsetHeight
+      }px`;
+      break;
+
+    case "Numpad4":
+      mouseSprite.style.left = `${
+        mouseSprite.getBoundingClientRect().left - mouseSprite.offsetWidth
+      }px`;
+      break;
+  }
+});
