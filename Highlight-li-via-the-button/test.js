970ebcf069bd49f40fa5d6eb6f489727637bb709
Kristina <kristino4ka_17@mail.ru>
2023-07-05 22:12:29 +0300

    change the condition

--- Highlight-li-via-the-button/test.js
+++ Highlight-li-via-the-button/test.js
@@ -11,9 +11,9 @@ list.addEventListener("click", function (event) {
   if (event.ctrlKey || event.metaKey) {
     // используем комбинацию клавиш, чтобы выделить элементы, даем им класс
     event.target.classList.add("selected");
-  } else {
-    // сбрасываем выделенные элементы
-    removeClass();
-    event.target.classList.add("selected");
+    return;
   }
+  // сбрасываем выделенные элементы
+  removeClass();
+  event.target.classList.add("selected");
 });
